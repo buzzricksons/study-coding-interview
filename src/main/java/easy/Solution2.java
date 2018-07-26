a2f1f4ea6f69a54add6795d7f1e5c11c39ecd6b9
buzzricksons <hcgoon@gmail.com>
2018-07-26 18:53:08 +0900

    add Solution2

--- src/main/java/easy/Solution2.java
+++ src/main/java/easy/Solution2.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 public class Solution2 {
     public static void main(String[] args) {
 
+//        Input: [1,2,3,4,5,6,7] and k = 3
+//        Output: [5,6,7,1,2,3,4]
+//        Explanation:
+//        rotate 1 steps to the right: [7,1,2,3,4,5,6]
+//        rotate 2 steps to the right: [6,7,1,2,3,4,5]
+//        rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
 
 
 
