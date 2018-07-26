64ff54176d5d9a261cf3be9a5296c96fa30b108e
buzzricksons <hcgoon@gmail.com>
2018-07-26 18:33:36 +0900

    add Rotate Array

--- src/main/java/leetcode/easy/Solution1.java
+++ src/main/java/leetcode/easy/Solution1.java
@@ -1,18 +1,37 @@
 package leetcode.easy;
 
+import java.util.Arrays;
+
 public class Solution1 {
     public static void main(String[] args) {
+        //array: [0, 1, 2, 3, 4, 4, 4]
+        //i: 5
+        System.out.println("i: "+removeDuplicates(new int[]{0,1,2,2,3,4,4 }));
+
+
+
+
+
 
     }
 
     //Remove Duplicates from Sorted Array
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         int i = nums.length > 0 ? 1 : 0;
-        for (int n : nums)
-            if (n > nums[i-1])
+        for (int n : nums) {
+            if (n > nums[i - 1]) {
                 nums[i++] = n;
+            }
+        }
+        System.out.println("array: "+Arrays.toString(nums));
         return i;
     }
 
+
+    //Rotate Array
+    public static void rotate(int[] nums, int k) {
+
+
+    }
 
 }
