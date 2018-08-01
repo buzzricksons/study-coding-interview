a9ccd45cf67b98f70cbc75e06a3e50e107084ea7
buzzricksons <hcgoon@gmail.com>
2018-08-01 20:41:02 +0900

    add Rotate Array

--- src/main/java/category/topinterviewquestions/easy/Array.java
+++ src/main/java/category/topinterviewquestions/easy/Array.java
@@ -2,11 +2,17 @@ package category.topinterviewquestions.easy;
 
 import java.util.Arrays;
 
-public class Solution1 {
+public class Array {
     public static void main(String[] args) {
         //array: [0, 1, 2, 3, 4, 4, 4]
         //i: 5
         System.out.println("i: "+removeDuplicates(new int[]{0,1,2,2,3,4,4 }));
+
+
+
+
+
+
     }
 
     //Remove Duplicates from Sorted Array
@@ -20,4 +26,32 @@ public class Solution1 {
         System.out.println("array: "+Arrays.toString(nums));
         return i;
     }
+
+
+
+
+
+    //Rotate Array
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+
+
+
+
 }
--- src/main/java/category/topinterviewquestions/easy/Solution2.java
+++ src/main/java/category/topinterviewquestions/easy/Solution2.java
@@ -1,4 +1,4 @@
-package topinterviewquestions.easy;
+package category.topinterviewquestions.easy;
 
 public class Solution2 {
     public static void main(String[] args) {
