6f097900b7cfe999649f5b0e256334720f9d97d8
HyungCheol Kim <hcgoon@gmail.com>
2018-08-06 12:54:22 +0900

    add Contains Duplicate

--- src/main/java/category/topinterviewquestions/easy/Array.java
+++ src/main/java/category/topinterviewquestions/easy/Array.java
@@ -1,8 +1,6 @@
 package category.topinterviewquestions.easy;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Array {
@@ -16,9 +14,17 @@ public class Array {
         //[7,1,2,3,4,5,6]
         //[6,7,1,2,3,4,5]
         //[5,6,7,1,2,3,4]
-        int[] param = new int[]{1,2,3,4,5,6,7 };
-        rotateByMe4(param, 3);
+//        int[] param = new int[]{1,2,3,4,5,6,7};
+//        rotateByMe4(param, 3);
 
+
+
+        int[] param1 = new int[]{1,2,3,1};
+        int[] param2 = new int[]{1,2,3,4};
+        int[] param3 = new int[]{1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicateByMe2(param1));
+        System.out.println(containsDuplicateByMe2(param2));
+        System.out.println(containsDuplicateByMe2(param3));
 
 
 
@@ -134,8 +140,35 @@ public class Array {
             end--;
         }
     }
+    //------------------------------------------------------------------------------------
 
 
+    //Contains DuplicateContains Duplicate
+    public static boolean containsDuplicateByMe1(int[] nums) {
+        for (int i=0; i < nums.length; i++) {
+            for (int j=0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicateByMe2(int[] nums) {
+        Set<Integer> temp = new HashSet<>();
+        for (int n : nums) {
+            if (!temp.add(n)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicateByMe3(int[] nums) {
+        Set<Integer> temp = new HashSet<>();
+        return Arrays.stream(nums).filter(n -> !temp.add(n)).findAny().isPresent();
+    }
     //------------------------------------------------------------------------------------
 
 
