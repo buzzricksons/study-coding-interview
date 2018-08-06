b3b2b89a0c7454c77e92be8720b7adf6c7426193
buzzricksons <hcgoon@gmail.com>
2018-08-06 22:15:43 +0900

    add PlusOne

---
+++ src/main/java/category/topinterviewquestions/easy/Array_PlusOne.java
@@ -0,0 +1,18 @@
+package category.topinterviewquestions.easy;
+
+public class Array_PlusOne {
+    public static void main(String[] args) {
+//        Input: [1,2,3]
+//        Output: [1,2,4]
+//        Explanation: The array represents the integer 123.
+        int[] param = {1, 2, 3};
+
+
+    }
+    public static int[] plusOneByMe(int[] digits) {
+
+
+
+        return new int[]{1};
+    }
+}
