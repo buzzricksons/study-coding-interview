38551159454b0eb9a289d1add8f1ed6841188dbb
buzzricksons <hcgoon@gmail.com>
2018-08-06 21:37:23 +0900

    add Move to zero

--- src/main/java/category/topinterviewquestions/easy/Array_MoveZeroes.java
+++ src/main/java/category/topinterviewquestions/easy/Array_MoveZeroes.java
@@ -5,18 +5,32 @@ import java.util.Arrays;
 public class Array_MoveZeroes {
     public static void main(String[] args) {
         int[] param = {0,1,0,3,12};
-        moveZeroes(param);
+        moveZeroesByMe(param);
+        moveZeroesByAnser(param);
 
     }
 
-    public static void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int add = 1;
-            while (nums[i] == 0 && i != nums.length - 1 && i + add <= nums.length - 1) {
-                int temp = nums[i];
-                nums[i] = nums[i + add];
-                nums[i + add] = temp;
-                add++;
+    public static void moveZeroesByMe(int[] nums) {
+        int noneZeroCount = 0;
+        for (int n : nums) {
+            if (n != 0) {
+                nums[noneZeroCount++] = n;
+            }
+        }
+        while (noneZeroCount < nums.length) {
+            nums[noneZeroCount++] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroesByAnser(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
         }
         System.out.println(Arrays.toString(nums));
