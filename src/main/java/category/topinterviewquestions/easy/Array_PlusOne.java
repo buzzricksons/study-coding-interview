881a3ecffa0b7f9ff21fd29d86e4f47b5783c2b8
buzzricksons <hcgoon@gmail.com>
2018-08-09 19:11:02 +0900

    edit plus one

--- src/main/java/category/topinterviewquestions/easy/Array_PlusOne.java
+++ src/main/java/category/topinterviewquestions/easy/Array_PlusOne.java
@@ -10,9 +10,39 @@ public class Array_PlusOne {
 
     }
     public static int[] plusOneByMe(int[] digits) {
+        if (digits.length == 1){
+            int result = digits[0] + 1;
+            if (result > 9) {
+                return new int[]{1,0};
+            } else {
+                digits[0] = digits[0] + 1;
+                return digits;
+            }
+        }
 
-
-
-        return new int[]{1};
+        int last = digits[digits.length - 1];
+        if (last + 1 > 9) {
+            digits[digits.length - 1] = 0;
+            for (int i = digits.length - 2;i >=0; i--){
+                int result = digits[i] + 1;
+                if (result > 9) {
+                    digits[i] = 0;
+                    if (i == 0) {
+                        int[] r = new int[digits.length+1];
+                        r[0] = 1;
+                        for (int j = 1; i < digits.length;i++) {
+                            r[j] = digits[j - 1];
+                        }
+                        return r;
+                    }
+                } else {
+                    digits[i] = result;
+                    break;
+                }
+            }
+        } else {
+            digits[digits.length - 1] = last + 1;
+        }
+        return digits;
     }
 }
