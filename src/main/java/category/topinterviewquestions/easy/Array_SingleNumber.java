947b67f5e12ede804f16c99e4cfa80e5f6941b95
HyungCheol Kim <hcgoon@gmail.com>
2018-08-17 20:15:00 +0900

    add array single number

---
+++ src/main/java/category/topinterviewquestions/easy/Array_SingleNumber.java
@@ -0,0 +1,21 @@
+package category.topinterviewquestions.easy;
+
+public class Array_SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1};//1
+        System.out.println(singleNumber(nums));
+
+
+    }
+    public static int singleNumber(int[] nums) {
+        int[] temp = nums.clone();
+
+        for(int i=0; i < nums.length; i++) {
+            for (int j=0; j < nums.length; j++) {
+
+            }
+        }
+    }
+
+
+}
