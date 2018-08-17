8c9166dd57ffe0ed71107d40f1b4afe6f8d08fd8
HyungCheol Kim <hcgoon@gmail.com>
2018-08-17 20:54:42 +0900

    edit array single number

--- src/main/java/category/topinterviewquestions/easy/Array_SingleNumber.java
+++ src/main/java/category/topinterviewquestions/easy/Array_SingleNumber.java
@@ -3,7 +3,7 @@ package category.topinterviewquestions.easy;
 public class Array_SingleNumber {
     public static void main(String[] args) {
         int[] nums = {2,3,2,1,3};//1
-        System.out.println(singleNumberByMe2(nums));
+        System.out.println(singleNumberByAnswer(nums));
 
 
     }
@@ -24,9 +24,27 @@ public class Array_SingleNumber {
         throw new IllegalArgumentException();
     }
 
-    public static int singleNumberByMe2(int[] nums) {
-
-        return 0;
+    public static int singleNumberByAnswer(int[] nums) {
+//        we use bitwise XOR to solve this problem :
+//
+//        first , we have to know the bitwise XOR in java
+//
+//        0 ^ N = N
+//        N ^ N = 0
+//        So..... if N is the single number
+//
+//        N1 ^ N1 ^ N2 ^ N2 ^..............^ Nx ^ Nx ^ N
+//
+//                = (N1^N1) ^ (N2^N2) ^..............^ (Nx^Nx) ^ N
+//
+//                = 0 ^ 0 ^ ..........^ 0 ^ N
+//
+//                = N
+        int result = 0;
+        for (int i = 0; i<nums.length; i++) {
+            result ^=nums[i];
+        }
+        return result;
     }
 
 
