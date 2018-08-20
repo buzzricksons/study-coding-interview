896712351e098ae76845febf9d34f4701babc25c
buzzricksons <hcgoon@gmail.com>
2018-08-20 19:09:37 +0900

    add linkedlist delete node

---
+++ src/main/java/category/topinterviewquestions/easy/LinkedList_DeleteNodeInALinkedList.java
@@ -0,0 +1,29 @@
+package category.topinterviewquestions.easy;
+
+public class LinkedList_DeleteNodeInALinkedList {
+    public static void main(String[] args) {
+//        Input: head = [4,5,1,9], node = 5
+//        Output: [4,1,9]
+//        Explanation: You are given the second node with value 5, the linked list
+//        should become 4 -> 1 -> 9 after calling your function.
+//        Example 2:
+//
+//        Input: head = [4,5,1,9], node = 1
+//        Output: [4,5,9]
+//        Explanation: You are given the third node with value 1, the linked list
+//        should become 4 -> 5 -> 9 after calling your function.
+
+    }
+
+    public static void deleteNode(ListNode node) {
+
+    }
+
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
