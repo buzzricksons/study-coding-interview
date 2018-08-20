1fe481941b81b937d0b63264266fa82997b974d5
buzzricksons <hcgoon@gmail.com>
2018-08-20 17:19:17 +0900

    add first unique character in a string

---
+++ src/main/java/category/topinterviewquestions/easy/String_FirstUniqueCharacterInAString.java
@@ -0,0 +1,12 @@
+package category.topinterviewquestions.easy;
+
+public class String_FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+
+    }
+    public static int firstUniqChar(String s) {
+
+        return 0;
+
+    }
+}
