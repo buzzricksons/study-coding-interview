2ee347f235e9842dde4a84cc50b07f4dff06bdab
buzzricksons <hcgoon@gmail.com>
2018-08-22 21:41:07 +0900

    add merge sorted array

--- src/main/java/category/topinterviewquestions/easy/SortingAndSearching_FirstBadVersion.java
+++ src/main/java/category/topinterviewquestions/easy/SortingAndSearching_FirstBadVersion.java
@@ -2,14 +2,14 @@ package category.topinterviewquestions.easy;
 
 public class SortingAndSearching_FirstBadVersion {
     public static void main(String[] args) {
-        System.out.println(firstBadVersion(2));
+        System.out.println(firstBadVersionByAnswer(2));
 
     }
 
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-    public static int firstBadVersion(int n) {
+    public static int firstBadVersionByAnswer(int n) {
         int start = 1, end = n;
         while (start < end) {
             int mid = start + (end-start) / 2;
---
+++ src/main/java/category/topinterviewquestions/easy/SortingAndSearching_MergeSortedArray.java
@@ -0,0 +1,15 @@
+package category.topinterviewquestions.easy;
+
+public class SortingAndSearching_MergeSortedArray {
+    public static void main(String[] args) {
+//        Input:
+//        nums1 = [1,2,3,0,0,0], m = 3
+//        nums2 = [2,5,6],       n = 3
+//
+//        Output: [1,2,2,3,5,6]
+
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+}
