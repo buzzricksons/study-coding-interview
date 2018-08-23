3ea63deeafda37d9dafd798a4a5c7b693726d157
buzzricksons <hcgoon@gmail.com>
2018-08-23 21:48:19 +0900

    add max depth

---
+++ src/main/java/category/topinterviewquestions/easy/Trees_ValidateBinarySearchTree.java
@@ -0,0 +1,24 @@
+package category.topinterviewquestions.easy;
+
+public class Trees_ValidateBinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+    public boolean isValidBST(TreeNode root) {
+        return false;
+
+    }
+
+   public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+   }
+}
