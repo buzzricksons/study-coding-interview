23b8eb78f1e9f09dd990da42838dbbd5fbfbc566
buzzricksons <hcgoon@gmail.com>
2018-08-23 20:07:29 +0900

    edit merge sorted array

--- src/main/java/category/topinterviewquestions/easy/SortingAndSearching_MergeSortedArray.java
+++ src/main/java/category/topinterviewquestions/easy/SortingAndSearching_MergeSortedArray.java
@@ -7,9 +7,57 @@ public class SortingAndSearching_MergeSortedArray {
 //        nums2 = [2,5,6],       n = 3
 //
 //        Output: [1,2,2,3,5,6]
+//        mergeByMe();
 
     }
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void mergeByMe(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
 
+        while (i >= 0 || j >= 0) {
+            if (i < 0) {
+                nums1[k--] = nums2[j--];
+            } else if (j < 0) {
+                nums1[k--] = nums1[i--];
+            } else if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public static void mergeByAnswer(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i >=0 && j>=0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j>=0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    public static void mergeByAnswer2(int[] nums1, int m, int[] nums2, int n) {
+        int p = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 < 0) {
+                nums1[p--] = nums2[p2--];
+            } else if (p2 < 0) {
+                nums1[p--] = nums1[p1--];
+            } else if (nums1[p1] < nums2[p2]) {
+                nums1[p--] = nums2[p2--];
+            } else {
+                nums1[p--] = nums1[p1--];
+            }
+        }
     }
 }
